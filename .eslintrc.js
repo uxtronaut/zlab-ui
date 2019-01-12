80e554dee6dffdca289f671b87f10244294a2550
Zach Payne <uxtronaut@gmail.com>
2019-01-12 16:21:35 -0700

    Update project packages, add humps, lodash, axios, and vue/vuex decorators/helpers

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/no-extraneous-dependencies': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': [2, { 'allow': ['_consts'] }],
   },
   parserOptions: {
     parser: 'typescript-eslint-parser',
